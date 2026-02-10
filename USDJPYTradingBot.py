b6497ced3865cda14278f6111a719cae318671ab
Jin407 <139321228+Jin407@users.noreply.github.com>
2026-02-10 08:18:48 +0800

    Refactor USDJPYTradingBot for H4 candlestick data
    Updated trading bot to use H4 candlestick data and refined order parameters. Enhanced comments for clarity on trading logic and strategies.

--- USDJPYTradingBot.py
+++ USDJPYTradingBot.py
@@ -24,124 +24,130 @@ headers = {
 }
 """
 
-# Define query parameters
+# Define query parameters used to get candlestick data for closing price
 params = {
-    #instrument we will be trading in
+    #forex pair we are trading
     "instrument": "USD_JPY",
-    #Time chart we will be trading in, minute chart in this instance
-    "granularity":"M1",
+    #candlestick timeframe
+    "granularity":"H4",
+    #number of candle sticks
     "count":1
 }
 
-#json object for buy orders
+#json object for buying instrument
 buy = {
     "order": {
-        #number of units we want to trade
+        #number of units we want to buy
         "units": 10000,
-        #instrument we want to trade
+        #what we are buying
         "instrument": "USD_JPY",
-        #Fill or kill order, either order gets filled entirely or cancelled
+        #type of market order
         "timeInForce": "FOK",
         "type": "MARKET",
         "positionFill": "DEFAULT",
-        #stop loss is defined based on market price later on in the algorithm
+        #stop loss to be defined based on closing price
         "stopLossOnFill": {
             "price": ""
         },
-        #take profit is defined based on market price later on in the algorithm
+        #take profit to be defined based on closing price
         "takeProfitOnFill":{
             "price": ""
         }
     }
 }
 
-#json object to exit buy orders
+
+#json object for exiting a buy position
 buyExit = {
     "order": {
-        #units should be opposigte of what we bought, to sell in the market
+        #units should be opposite of buy order to sell off all units we are holding
         "units": -10000,
-        #instrument we are trading in
+        #type of instrument we are trading
         "instrument": "USD_JPY",
-        #Fill or kill order
+        #type of market order
         "timeInForce": "FOK",
         "type": "MARKET",
         "positionFill": "DEFAULT" 
     }
 }
 
-#json object to enter into sell orders
+#json object for entering a sell position
 sell = {
     "order": {
-        #number of units we are short selling
+        #number of units we are selling to be set in negative numbers
         "units": -10000,
-        #instrument we are trading
+        #type of instrument we are selling
         "instrument": "USD_JPY",
-        #Fill or kill order
+        #type of market order
         "timeInForce": "FOK",
         "type": "MARKET",
         "positionFill": "DEFAULT",
-        #Stop loss will be defined based on market price later on
-         "stopLossOnFill": {
+        #stop loss to be defined based on closing price
+        "stopLossOnFill": {
             "price": ""
         },
-        #Take profit will be defined based on market price later on
+        #take profit to be defined based on closing price
         "takeProfitOnFill":{
             "price": ""
         }   
     }
 }
 
-#json object to exit sell orders
+#json object for exiting a sell position
 sellExit = {
     "order": {
-        #number of units should be opposite to our sell orders to exit the position, buy back instruments we short sold previously
+        #units is based on sell position, we are buying back the units we sold previously
         "units": 10000,
-        #instrument we are trading in
+        #type of instrument we are trading
         "instrument": "USD_JPY",
-        #Fill or kill order
+        #type of market order
         "timeInForce": "FOK",
         "type": "MARKET",
         "positionFill": "DEFAULT"
     }
 }
 
-#method to calculate macd
+#method to calculate MACD
 def calculate_macd(instrument):
+
     MACDparams ={
-        #Time chart we are looking at, in this case, minute chart
-        "granularity":"M1"
+        #candlestick timeframe we are looking at
+        "granularity":"H4"
     }
-    #request to OANDA's rest api for recent candlestick data in the minute chart
+    #API request to get recent candlestick data
     request = oandapyV20.endpoints.instruments.InstrumentsCandles(instrument=instrument, params=MACDparams)
     response = api.request(request)
 
-    #store the recent prices of USDJPY in a dictionary
+    #Array to hold recent price data
     prices = [float(candle['mid']['c']) for candle in response['candles']]
 
-    #using talib to calculate MACD based on recent USD JPY prices, fast, slow and signal period are default settings and can be adjusted
-    macd, signal, _ = talib.MACD(np.array(prices), fastperiod=12, slowperiod=26, signalperiod=9)
-
-    #return most recent macd
-    return macd[-1]
+    #use talib to calculate macd based on price data, this would produce 3 arrays macd line values, macd signal line values and macd histograms
+    #fast period, slowperiod, signalperiod determines how many candlesticks we are looking at for macd, lesser value would react faster to changes while higher value produce lesser false signals
+    macd, signal, histogram = talib.MACD(np.array(prices), fastperiod=12, slowperiod=26, signalperiod=9)
+    
+    #return most recent macd value
+    return macd[-1], signal[-1]
 
 #method to calculate bollinger bands
 def calculate_bollinger_bands(instrument):
     bbparams ={
-        #Time chart we are looking at, in this case, minute chart
-        "granularity":"M1"
+        #candlestick time frame
+        "granularity":"H4"
     }
-    #request to OANDA's rest api for recent candlestick data in the minute chart
+
+    #API request to get recent candlestick data
     request = oandapyV20.endpoints.instruments.InstrumentsCandles(instrument=instrument, params=bbparams)
     response = api.request(request)
 
-    #store the recent prices of USDJPY in a dictionary
+    #Array to hold recent prices
     prices = [float(candle['mid']['c']) for candle in response['candles']]
 
-    #using talib to calculate bollinger bands, settings for time period to look at and standard deviation from middle band is set to 2 for both upper and lower band by default
-    #matype=0 means we are using simple moving average to calculate bollinger bands
-    upper_band, middle_band, lower_band = talib.BBANDS(np.array(prices), timeperiod=14, nbdevup=2, nbdevdn=2, matype=0)
+    # Calculate Bollinger Bands using talib which would return 3 array fo values, the upper, middle and lower bands values
+    #params are timeperiod which determine the number of candlesticks we look at and nbdevup and nbdevdn are standard deviations between the upper and lower bands from the middle band
+    #matype 0 stands for simple moving average
+    upper_band, middle_band, lower_band = talib.BBANDS(np.array(prices), timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
     
-    #return most recent bollinger band data
+    #return latest bollinger band data
     return upper_band[-1], middle_band[-1], lower_band[-1]
 
 
@@ -150,115 +156,99 @@ buy_trade = False
 sell_trade = False
 pmacd = 0
 cmacd = 0
+psignal = 0
+csignal = 0
 closing_price = 0
-file = open("error.txt",'a')
+squeeze_threshold = 0.9
+retry_count = 0
 current_time = time.localtime()
-#this would ensure algorithm runs at the start of the minute and data is syncronized with the market
-time.sleep(60-current_time.tm_sec)
 
 
 while True:
+    params = {
+    #forex pair we are trading
+    "instrument": "USD_JPY",
+    #candlestick timeframe
+    "granularity":"H4",
+    #number of candle sticks
+    "count":1
+    }
     #API request to get candlestick data 
     response = requests.get(url, headers=headers, params=params)
     candle_data = response.json()
 
-    #writing errors to file
-    if(response.status_code != 200):
-        file.write(response.status_code)
-        file.write(response.content)
-
     #using candlestick data from above to determine opening, closing price, candle_data is structured 
-    opening_price = float(candle_data['candles'][0]['mid']['o'])
     closing_price = float(candle_data['candles'][0]['mid']['c'])
 
     #API request to get positions data
     position = requests.get(positions_url, headers=headers)
     position_data = position.json()
 
-    if(position.status_code != 200):
-        file.write(str(position.status_code))
-        file.write(str(position.content))
+    if response.status_code >= 300 or position.status_code >= 300:
+
+        retry_count += 1
+        if retry_count >= 5:
+            break
+
+        time.sleep(60)
+        continue
+
+    else:
+        retry_count = 0
 
     #calculating macd and bollinger band
     upper_band, middle_band, lower_band = calculate_bollinger_bands(instrument)
-    cmacd = calculate_macd(instrument)
+    cmacd, csignal = calculate_macd(instrument)
+
+    macd_zero_cross_above = pmacd < 0 and cmacd > 0
+    macd_zero_cross_below = pmacd > 0 and cmacd < 0
+    price_at_lower_band = closing_price <= lower_band
+    price_at_higher_band = closing_price >= upper_band
+    macd_bullish_cross =  pmacd <= psignal and cmacd > csignal
+    macd_bearish_cross =  pmacd >= psignal and cmacd < csignal
+    bollinger_band_squeeze = (upper_band - lower_band) < squeeze_threshold
 
     #determine if there are open buy positions
-    if int(position_data['position']['long']['units']) > 0:
-        buy_trade = True
-    else:
-        buy_trade = False
-
+    buy_trade = int(position_data['position']['long']['units']) > 0
     #determine if there are any open sell positions
-    if int(position_data['position']['short']['units']) < 0:
-        sell_trade = True
-    else:
-        sell_trade = False
+    sell_trade = int(position_data['position']['short']['units']) < 0
+    
     
     #if there are no positions opened, we are looking to enter into a position
     if(buy_trade == False and sell_trade == False):
-        #sell entry strategy being candlestick that opens above the upper band and closes back into the bollinger range when macd is below 0 we are looking to sell
-        if closing_price > upper_band and cmacd < 0:
-            #define stop loss 5 pips above the selling price
-            sell["order"]["stopLossOnFill"]["price"]= str(round(closing_price + 0.05,3))
-            #define take profit 70 pips below selling price
-            sell["order"]["takeProfitOnFill"]["price"]= str(round(closing_price - 0.7))
+        #buy entry strategy
+        if macd_zero_cross_above and bollinger_band_squeeze:
+            #define stop loss 100 pips below the buying price, note USD JPY pip is on 2 decimal place instead of 4
+            buy["order"]["stopLossOnFill"]["price"]= str(round(closing_price - 1,3))
+            #define take profit 150 pips above the buying price, note USD JPY pip is on 2 decimal place instead of 4
+            buy["order"]["takeProfitOnFill"]["price"]= str(round(closing_price + 1.5,3))
+            #API request to buy order
+            buy_entry = requests.post(orderURL, headers=headers, json=buy)
+            
+        #sell entry strategy
+        elif macd_zero_cross_below and bollinger_band_squeeze:
+            #define stop loss 100 pips above the selling price
+            sell["order"]["stopLossOnFill"]["price"]= str(round(closing_price + 1,3))
+            #define take profit 150 pips below selling price
+            sell["order"]["takeProfitOnFill"]["price"]= str(round(closing_price - 1.5,3))
             #API request to sell order
             sell_entry = requests.post(orderURL, headers=headers, json=sell)
 
-            if(sell_entry.status_code == 201):
-                #sell trade successfully placed
-                print("Sell trade successfully placed")
-            else:
-                #error handling for sell trade unsuccessful
-                file.write("Sell_entry ", str(sell_entry.status_code))
-                file.write(str(sell_entry.content))
-
-        #buy entry strategy being candlestick that opens below the lower band and closes back into the bollinger range when macd is above 0 we are looking to buy
-        elif closing_price > lower_band and cmacd > 0:
-            #define stop loss 5 pips below the buying price, note USD JPY pip is on 2 decimal place instead of 4
-            buy["order"]["stopLossOnFill"]["price"]= str(round(closing_price - 0.05,3))
-            #define take profit 70 pips above the buying price, note USD JPY pip is on 2 decimal place instead of 4
-            buy["order"]["takeProfitOnFill"]["price"]= str(round(closing_price + 0.7,3))
-            #API request to buy order
-            buy_entry = requests.post(orderURL, headers=headers, json=buy)
-            if(buy_entry.status_code == 201):
-                #buy trade successfully place
-                print("Buy trade successfully placed")
-            else:
-                #error handling for buy trade unsuccessful
-                file.write("buy_entry ", str(buy_entry.status_code))
-                file.write(str(buy_entry.content))
     #When there is a position opened either buy or sell we are looking to exit            
     else:
-        #exit strategy for sell orders being macd crossing above 0 signifying a potential change in trend from bearish to bullish
-        if (pmacd < 0 and cmacd > 0) and sell_trade == True:
-            #API request to exit sell order
-            sell_exit = requests.post(orderURL, headers=headers, json=sellExit)
-            if(sell_exit.status_code == 201):
-                #sell trade successfully exited
-                print("Sell trade successfully exited")
-            else:
-                #error handling for sell trade exit
-                file.write("sell_exit ", str(sell_exit.status_code))
-                file.write(str(sell_exit.content))
-        #exit strategy for sell orders being macd crossing below 0 signifying a potential change in trend from bullish to bearish
-        elif (pmacd > 0 and cmacd < 0) and buy_trade == True:
+
+        #exit strategy for buy orders
+        if (price_at_higher_band or macd_bearish_cross) and buy_trade == True:
             #API request to exit buy order
             buy_exit = requests.post(orderURL, headers=headers, json=buyExit)
-            if(buy_exit.status_code == 201):
-                #buy trade successfully exited
-                print("Buy trade successfully exited")
-            else:
-                #error handling for buy trade exit
-                file.write("buy_exit ", str(buy_exit.status_code))
-                file.write(str(buy_exit.content))
+
+        #exit strategy for sell orders
+        elif (price_at_lower_band or macd_bullish_cross) and sell_trade == True:
+            #API request to exit sell order
+            sell_exit = requests.post(orderURL, headers=headers, json=sellExit)
 
     #set past macd to current macd so that in the next iteration after current macd is recalculated it can be compared to past macd 1 iteration before it
     pmacd = cmacd
-    #buffer to ensure our algorithm is still running
+    psignal = csignal
     print(closing_price)
-
-    #API request takes up to 2 seconds to execute as such we let the algorithm sleep for only 58 seconds to ensure each iteration begins with a new minute
-    time.sleep(58)
-
+    time.sleep(14400)
